3c322340814520372a411a3f14cf16f747ed378a
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 11:58:05 +0545

    display below mgs and bring it at the top

--- app/src/main/java/Adapter/ViewHolder.java
+++ app/src/main/java/Adapter/ViewHolder.java
@@ -1,17 +1,40 @@
 package Adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.e.recyclerview.R;
 
+import java.util.List;
+
 import de.hdodenhof.circleimageview.CircleImageView;
+import model.Contacts;
 
-public class ViewHolder {
+public class ViewHolder extends RecyclerView.Adapter<ViewHolder.ContactsViewHolder>{
 
-    public class ContactsAdapter extends RecyclerView.Adapter<>
+        Context mContect;
+        List<Contacts> contactsList;
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder contactsViewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ContactsViewHolder extends RecyclerView.ViewHolder{
 
             CircleImageView imgProfile;
